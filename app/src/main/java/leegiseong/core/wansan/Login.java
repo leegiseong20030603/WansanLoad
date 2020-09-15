1cc5cf7944a9adafdbb0ef7a6a6586b981b815cc
Lee Gi Seong <shine01035857458@gmail.com>
2020-09-15 20:23:27 +0900

    2020-09-13

--- app/src/main/java/leegiseong/core/wansan/Login.java
+++ app/src/main/java/leegiseong/core/wansan/Login.java
@@ -3,6 +3,7 @@ package leegiseong.core.wansan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -11,15 +12,25 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class Login extends AppCompatActivity {
 
     TextInputEditText input_id, input_pw;
+    EditText ID, PW;
     TextInputLayout id_layout, pw_layout;
-    Button Register;
+    Button Register, Login;
+    Retrofit retrofit;
+    RetrofitConfig retrofitConfig;
+    Interface Interface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +41,47 @@ public class Login extends AppCompatActivity {
         id_layout = findViewById(R.id.id_layout);
         pw_layout = findViewById(R.id.pw_layout);
         Register = findViewById(R.id.Register);
-
+        Login = findViewById(R.id.Login);
+        retrofitConfig = new RetrofitConfig();
+        retrofit = retrofitConfig.getRetrofit();
+        Interface = retrofit.create(Interface.class);
         id_layout.setErrorTextColor(ColorStateList.valueOf(getResources().getColor(R.color.errorTextColor)));
         pw_layout.setErrorTextColor(ColorStateList.valueOf(getResources().getColor(R.color.errorTextColor)));
+        ID = id_layout.getEditText();
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getID = ID.getText().toString();
+                String getPW = PW.getText().toString();
+                if (getID.isEmpty()){
+                    Toast.makeText(Login.this, "아이디를 넣어주세요.",Toast.LENGTH_LONG).show();
+                }else if (getPW.isEmpty()){
+                    Toast.makeText(Login.this, "비밀번호를 넣어주세요.",Toast.LENGTH_LONG).show();
+                }else {
+                    Interface.Login(getID, getPW).enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            User responseBody = response.body();
+                            if (responseBody.getResponse()){
+                                String[] TitleItem = getResources().getStringArray(R.array.sharedpreferences);
+                                SharedPreferences sharedPreferences = getSharedPreferences(TitleItem[0],0);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(TitleItem[1],responseBody.getId());
+                                editor.putString(TitleItem[2],responseBody.getPw());
+                                editor.apply();
+                            }else {
+
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+
+                        }
+                    });
+                }
+            }
+        });
 
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,11 +90,10 @@ public class Login extends AppCompatActivity {
                 startActivity(Register);
             }
         });
-        EditText ID_EDIT = id_layout.getEditText();
 
         pw_layout.setPasswordVisibilityToggleEnabled(true);
 
-        ID_EDIT.addTextChangedListener(new TextWatcher() {
+        ID.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
--- app/src/main/java/leegiseong/core/wansan/RegisterMessageAuth.java
+++ app/src/main/java/leegiseong/core/wansan/RegisterMessageAuth.java
@@ -1,5 +1,9 @@
 package leegiseong.core.wansan;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 import android.util.Log;
@@ -9,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +23,7 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -29,7 +35,12 @@ public class RegisterMessageAuth extends Fragment {
     RegisterViewModel viewModel;
     PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     Button sendAuthCode, MessageAuth;
-    EditText phoneNumber;
+    EditText phoneNumber, authCode;
+    NoSwipeableViewpager registerViewpager;
+    String SMS_CODE;
+    Dialog dialog;
+    String getPhoneNumber;
+    PhoneAuthProvider.ForceResendingToken token;
     LinearLayout authCodeContainer;
 
     @Nullable
@@ -37,26 +48,66 @@ public class RegisterMessageAuth extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.register_messageauth,container,false);
         sendAuthCode = view.findViewById(R.id.sendAuthCode);
+        sendAuthCode.setText("인증번호 전송");
+        authCode = view.findViewById(R.id.authCode);
         phoneNumber = view.findViewById(R.id.phoneNumber);
         MessageAuth = view.findViewById(R.id.MessageAuth);
         authCodeContainer = view.findViewById(R.id.authCodeContainer);
-        sendAuthCode.setOnClickListener(new View.OnClickListener() {
+        registerViewpager = getActivity().findViewById(R.id.Register_viewPager);
+        sendAuthCode.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String getPhoneNumber = phoneNumber.getText().toString();
+                getPhoneNumber = phoneNumber.getText().toString();
                 if (getPhoneNumber.length() < 11){
                     Toast.makeText(view.getContext(), "전화번호을 옳바르게 넣어주세요.",Toast.LENGTH_LONG).show();
                 }else{
-                    String formatNumberToE164 = PhoneNumberUtils.formatNumberToE164(getPhoneNumber,"KR");
-                    Log.d("phone", formatNumberToE164);
-                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                            formatNumberToE164,
-                            30,
-                            TimeUnit.SECONDS,
-                            getActivity(),
-                            mCallbacks
-                    );
-                    authCodeContainer.setVisibility(View.VISIBLE);
+                    String formatNumberToE164 = PhoneNumberUtils.formatNumberToE164(getPhoneNumber, "KR");
+                    if (authCodeContainer.getVisibility() == View.VISIBLE){
+                        resendVerificationCode(formatNumberToE164, token);
+                    }else if (authCodeContainer.getVisibility() == View.GONE) {
+                        Log.d("phone", formatNumberToE164);
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                formatNumberToE164,
+                                120,
+                                TimeUnit.SECONDS,
+                                getActivity(),
+                                mCallbacks
+                        );
+                        authCodeContainer.setVisibility(View.VISIBLE);
+                        sendAuthCode.setText("인증번호 재전송");
+                    }
+                }
+            }
+        });
+
+        MessageAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getAuthCode = authCode.getText().toString();
+                if (getAuthCode.length() == 6){
+                    if (getAuthCode.equals(SMS_CODE)){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                        View OKView = inflater.inflate(R.layout.ok, null);
+                        builder.setView(OKView);
+                        Button OK = OKView.findViewById(R.id.OK);
+                        TextView messageTextView = OKView.findViewById(R.id.messageTextView);
+                        messageTextView.setText("핸드폰 인증에 성공하셨습니다.");
+                        OK.setText("확인");
+                        OK.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                dialog.dismiss();
+                                viewModel.setPhoneNumber(getPhoneNumber);
+                                registerViewpager.setCurrentItem(2);
+                            }
+                        });
+                        dialog = builder.create();
+                        dialog.setCanceledOnTouchOutside(false);
+                        dialog.show();
+                    }
+                }else {
+                    Toast.makeText(getActivity(),"인증코드 6자리을 넣어주세요",Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -70,7 +121,7 @@ public class RegisterMessageAuth extends Fragment {
 
             @Override
             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                String SMS_CODE = phoneAuthCredential.getSmsCode();
+                SMS_CODE = phoneAuthCredential.getSmsCode();
                 Log.d("RegisterMessageAuth : ", SMS_CODE);
             }
 
@@ -79,8 +130,7 @@ public class RegisterMessageAuth extends Fragment {
                 Log.w("TAG", e.getMessage(),e);
 
                 if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                    // Invalid request
-                    // ...
+                    Toast.makeText(getActivity(),"옳바르지 않는 전화번호입니다.\n   다시 입력해주세요",Toast.LENGTH_LONG).show();
                 } else if (e instanceof FirebaseTooManyRequestsException) {
                     // The SMS quota for the project has been exceeded
                     // ...
@@ -88,12 +138,22 @@ public class RegisterMessageAuth extends Fragment {
             }
 
             @Override
-            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
-
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken t) {
                 Log.d("TAG", "onCodeSent:" + verificationId);
+                token = t;
             }
         };
         return view;
+    }
+
+    private void resendVerificationCode(String phoneNumber, PhoneAuthProvider.ForceResendingToken token) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                getActivity(),               // Activity (for callback binding)
+                mCallbacks,         // OnVerificationStateChangedCallbacks
+                token);             // ForceResendingToken from callbacks
     }
 
     @Override
